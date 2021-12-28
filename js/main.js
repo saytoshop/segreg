ec577dd07ffbb6137b6500b777751b7e2854e895
sshadowww <2580744@gmail.com>
2021-12-28 17:58:50 +0300

    mobile main

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var app = new Vue({
+    el: '#header',
+    created() {
+        // this.fetchData();
+    },
+    data: {
+        menuActive: false,
+    },
+    computed: {
+        menuHeight() {
+            h = -55 + document.documentElement.clientHeight - document.getElementById('authForm').offsetHeight - document.getElementById('headerTop').offsetHeight;
+            console.log(h);
+            return h + 'px';
+
+        }
+
+    },
+    methods: {
+
+        toggle() {
+            //      console.log(document.getElementById('authForm').offsetHeight);
+            this.menuActive = !this.menuActive;
+            if (this.menuActive) {
+                window.scrollTo(0, document.body.scrollHeight);
+
+            } else {
+                window.scrollTo(0, 0);
+            }
+        }
+    }
+
+
+
+});
+
+var main = new Vue({
+    el: '#main',
+    created() {
+        // this.fetchData();
+    },
+    data: {
+        menuActive: false,
+    },
+
+
+
+
+
+
+});
